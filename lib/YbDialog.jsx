da7b6f297dae4220891cdf9c2262f142450021aa
SKY-20170813HTR\Administrator <250751294@qq.com>
2019-01-04 15:28:50 +0800

    init

---
+++ lib/YbDialog.jsx
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+import { is, fromJS } from 'immutable';
+import ReactDOM from 'react-dom';
+import ReactCSSTransitionGroup from 'react-addons-css-transition-group';
+import "./YbDialog.css";
+
+
+class YbDialog extends Component {
+    render() {
+        return (
+            <div>
+
+            </div>
+        )
+    }
+}
+
+export default YbDialog;
